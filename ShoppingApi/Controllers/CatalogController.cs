e27f7995a3f994b357002009e89643358edfd483
Jeffry Gonzalez <jeff@hypertheory.com>
2020-10-12 15:11:59 -0400

    before monday break

---
+++ ShoppingApi/Controllers/CatalogController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Controllers
+{
+    public class CatalogController : ControllerBase
+    {
+        private readonly ShoppingDataContext _context;
+
+        public CatalogController(ShoppingDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("catalog")]
+        public async Task<ActionResult> GetFullCatalog()
+        {
+            var response = await _context.ShoppingItems.Where(item => item.InInventory)
+                .ToListAsync();
+
+            return Ok(response);
+        }
+    }
+}
