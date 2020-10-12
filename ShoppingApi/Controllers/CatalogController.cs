15b4b8fb151ad04a65ef1437eb9b77a40078514e
Jeffry Gonzalez <jeff@hypertheory.com>
2020-10-12 16:36:17 -0400

    almost done

--- ShoppingApi/Controllers/CatalogController.cs
+++ ShoppingApi/Controllers/CatalogController.cs
@@ -1,27 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using ShoppingApi.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ShoppingApi.Models.Catalog;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace ShoppingApi.Controllers
 {
     public class CatalogController : ControllerBase
     {
         private readonly ShoppingDataContext _context;
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
+        private readonly MapperConfiguration _mapperConfig;
 
-        public CatalogController(ShoppingDataContext context)
+        public CatalogController(ShoppingDataContext context, IConfiguration config, IMapper mapper, MapperConfiguration mapperConfig)
         {
             _context = context;
+            _config = config;
+            _mapper = mapper;
+            _mapperConfig = mapperConfig;
         }
 
         [HttpGet("catalog")]
         public async Task<ActionResult> GetFullCatalog()
         {
-            var response = await _context.ShoppingItems.Where(item => item.InInventory)
+            var data = await _context
+                .ShoppingItems
+                .AsNoTracking()
+                .TagWith("catalog#getfullcatalog")
+                .Where(item => item.InInventory)
+                .ProjectTo<GetCatalogResponseSummaryItem>(_mapperConfig)
                 .ToListAsync();
+
+            var response = new GetCatalogResponse
+            {
+                Data = data
+            };
+
 
             return Ok(response);
         }
